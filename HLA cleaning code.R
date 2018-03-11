b4a0141a46d7f2f0a26f21c798febadc32f303ca
Kikinator5000 <kirsten.anderson@ucdenver.edu>
2018-03-11 16:37:27 -0700

    Eliminated redundancy and only show final functions with examples

--- HLA cleaning code.R
+++ HLA cleaning code.R
@@ -7,31 +7,6 @@
 
 stupid_df <- read.csv("stupid_df.csv", header=TRUE, stringsAsFactors = FALSE)
 
-#snippet works
-#stupid_df[,1] <- gsub("/.*","",stupid_df[,1])
-
-# WORKS!!!
-resolveAmbiguity <- function(df){
-  for(c in 1:length(colnames(df))){
-    df[,c] <- gsub("/.*","", df[,c])
-  }
-  return(df)
-}
-
-#test
-stupid_df <- resolveAmbiguity(stupid_df)
-
-# Rewritten to allow you to specify starting column
-resolveAmbiguity <- function(df,x=1){
-  for(c in x:length(colnames(df))){
-    df[,c] <- gsub("/.*","", df[,c])
-  }
-  return(df)
-}
-
-#test
-stupid_df <- resolveAmbiguity(stupid_df,2)
-
 # Same function, but you can specify the character to replace and the starting column
 resolveAmbiguity <- function(df, x = 1, char){
   for(c in x:length(colnames(df))){
@@ -40,17 +15,12 @@ resolveAmbiguity <- function(df, x = 1, char){
   return(df)
 }
 
-#Test
+# Example
 stupid_df <- resolveAmbiguity(stupid_df, x = 2, char = "/.*")
 
-# Add some empty homozygous calls
-stupid_df <- read.csv("stupid_df.csv", header=TRUE, stringsAsFactors = FALSE)
+# When a cell contains a "-" it indicates homozygosity. 
+# This function replaces the "-" with the allele designated in the column to it's left
 
-stupid_df[1,2] <- "-"
-stupid_df[3,4] <- "-"
-stupid_df[4,6] <- "-"
-
-# Now it works
 copyHomozygous <- function (df){
   for(i in 1:length(row.names(df))){
     for(c in 1:length(colnames(df))){
@@ -67,6 +37,5 @@ copyHomozygous <- function (df){
   return(df)
 }
 
-# assign the output of the function to a new data frame
-stupid_df <- copyHomozygous(stupid_df)
+
 
